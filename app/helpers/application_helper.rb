62f60981918d99823f71b49f1aea00658aa6b16a
Chad Tarpey <chadtarpey@gmail.com>
2022-01-13 14:38:31 -0600

    added a helper for full title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+    # returns a full title on a per page basis
+
+# method defintion, optional argument
+    def full_title(page_title = ' ')
+        #creation and assigning a variable 
+        base_title = 'Orbit Feedback Application'
+        # boolean test
+        if page_title.empty? 
+
+            # implicit return 
+            base_title
+        else
+
+            # string concatenation
+            page_title + " | " + base_title
+        end
+
+   
+    end
 end
