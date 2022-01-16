ff4988accd37459e89b5d9f6249eff39cce19651
Chad Tarpey <chadtarpey@gmail.com>
2022-01-16 15:12:17 -0600

    added flash and test for user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,9 @@ class UsersController <  ApplicationController
   def create 
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to Orbit"
       #handle a successfull save
+      redirect_to @user
     else
       render 'new'
     end
